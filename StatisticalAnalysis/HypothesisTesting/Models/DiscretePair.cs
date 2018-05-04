c18d5099af2c0fe49bf9bea54276f23610cf5d46
Dmitry <omigoneo@gmail.com>
2018-05-04 23:48:04 +0300

    up

--- StatisticalAnalysis/HypothesisTesting/Models/DiscretePair.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StatisticalAnalysis.HypothesisTesting.Models
-{
-    public class DiscretePair
-    {
-        public int Variant { get; set; }
-        public int Value { get; set; }
-    }
-}
